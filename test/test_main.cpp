fd2320ab6c4e1dc786896ca9f9189ac4b51098eb
diyor <diyorsattarov7@gmail.com>
2023-09-05 13:47:58 -0500

    Added json route and enabled cross origin request

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,16 +3,33 @@
 #include "../extern/httplib.h"
 #include <spdlog/spdlog.h>
 
-int main(void)
-{
+int main(void) {
   httplib::Server svr;
+
+  svr.set_default_headers({
+    {"Access-Control-Allow-Origin", "http://localhost:3000"},
+    {"Access-Control-Allow-Methods", "GET, POST, OPTIONS"},
+    {"Access-Control-Allow-Headers", "Origin, Content-Type, Accept"}
+});
+
   // Initialize the logger
   spdlog::set_level(spdlog::level::debug);
   spdlog::info("Server starting...");
 
+  // Handle "/hi" route with text/plain content
   svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
     res.set_content("Hello World!", "text/plain");
   });
+
+  // Handle "/json" route with application/json content
+  svr.Get("/json", [](const httplib::Request &, httplib::Response &res) {
+    // Create a JSON response
+    std::string json_content = "{\"message\": \"Hello JSON!\"}";
+
+    // Set CORS headers for this specific route
+    res.set_header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.set_content(json_content, "application/json");
+});
 
   svr.listen("localhost", 8080);
 }
